47daa256374f104cdde649e3a5da548e8a5721f0
Pei-Tang Huang <tangtheone@gmail.com>
2017-03-05 22:31:38 +0800

    JDL import.

--- src/main/java/com/tj/kvasir/config/CacheConfiguration.java
+++ src/main/java/com/tj/kvasir/config/CacheConfiguration.java
@@ -42,6 +42,38 @@ public class CacheConfiguration {
             cm.createCache(com.tj.kvasir.domain.User.class.getName() + ".authorities", jcacheConfiguration);
             cm.createCache(com.tj.kvasir.domain.PersistentToken.class.getName(), jcacheConfiguration);
             cm.createCache(com.tj.kvasir.domain.User.class.getName() + ".persistentTokens", jcacheConfiguration);
+            cm.createCache(com.tj.kvasir.domain.QuestionChoice.class.getName(), jcacheConfiguration);
+            cm.createCache(com.tj.kvasir.domain.QuestionChoice.class.getName() + ".options", jcacheConfiguration);
+            cm.createCache(com.tj.kvasir.domain.QuestionChoice.class.getName() + ".categories", jcacheConfiguration);
+            cm.createCache(com.tj.kvasir.domain.QuestionChoice.class.getName() + ".images", jcacheConfiguration);
+            cm.createCache(com.tj.kvasir.domain.CategoryAcademicYear.class.getName(), jcacheConfiguration);
+            cm.createCache(com.tj.kvasir.domain.CategoryNode.class.getName(), jcacheConfiguration);
+            cm.createCache(com.tj.kvasir.domain.CategoryNode.class.getName() + ".trueOrFalses", jcacheConfiguration);
+            cm.createCache(com.tj.kvasir.domain.CategoryNode.class.getName() + ".choices", jcacheConfiguration);
+            cm.createCache(com.tj.kvasir.domain.CategoryNode.class.getName() + ".essays", jcacheConfiguration);
+            cm.createCache(com.tj.kvasir.domain.CategoryNode.class.getName() + ".groups", jcacheConfiguration);
+            cm.createCache(com.tj.kvasir.domain.CategoryPublisher.class.getName(), jcacheConfiguration);
+            cm.createCache(com.tj.kvasir.domain.CategorySemester.class.getName(), jcacheConfiguration);
+            cm.createCache(com.tj.kvasir.domain.CategorySubject.class.getName(), jcacheConfiguration);
+            cm.createCache(com.tj.kvasir.domain.CategoryGrade.class.getName(), jcacheConfiguration);
+            cm.createCache(com.tj.kvasir.domain.QuestionChoiceOption.class.getName(), jcacheConfiguration);
+            cm.createCache(com.tj.kvasir.domain.QuestionChoiceOption.class.getName() + ".images", jcacheConfiguration);
+            cm.createCache(com.tj.kvasir.domain.QuestionEssay.class.getName(), jcacheConfiguration);
+            cm.createCache(com.tj.kvasir.domain.QuestionEssay.class.getName() + ".categories", jcacheConfiguration);
+            cm.createCache(com.tj.kvasir.domain.QuestionEssay.class.getName() + ".images", jcacheConfiguration);
+            cm.createCache(com.tj.kvasir.domain.QuestionGroup.class.getName(), jcacheConfiguration);
+            cm.createCache(com.tj.kvasir.domain.QuestionGroup.class.getName() + ".choices", jcacheConfiguration);
+            cm.createCache(com.tj.kvasir.domain.QuestionGroup.class.getName() + ".trueFalses", jcacheConfiguration);
+            cm.createCache(com.tj.kvasir.domain.QuestionGroup.class.getName() + ".essays", jcacheConfiguration);
+            cm.createCache(com.tj.kvasir.domain.QuestionGroup.class.getName() + ".categories", jcacheConfiguration);
+            cm.createCache(com.tj.kvasir.domain.QuestionTrueFalse.class.getName(), jcacheConfiguration);
+            cm.createCache(com.tj.kvasir.domain.QuestionTrueFalse.class.getName() + ".categories", jcacheConfiguration);
+            cm.createCache(com.tj.kvasir.domain.QuestionTrueFalse.class.getName() + ".images", jcacheConfiguration);
+            cm.createCache(com.tj.kvasir.domain.ResourceImage.class.getName(), jcacheConfiguration);
+            cm.createCache(com.tj.kvasir.domain.ResourceImage.class.getName() + ".choices", jcacheConfiguration);
+            cm.createCache(com.tj.kvasir.domain.ResourceImage.class.getName() + ".choiceOptions", jcacheConfiguration);
+            cm.createCache(com.tj.kvasir.domain.ResourceImage.class.getName() + ".trueFalses", jcacheConfiguration);
+            cm.createCache(com.tj.kvasir.domain.ResourceImage.class.getName() + ".essays", jcacheConfiguration);
             // jhipster-needle-ehcache-add-entry
         };
     }

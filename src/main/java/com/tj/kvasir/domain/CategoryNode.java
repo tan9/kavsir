ea8411427359124de11fca352901b0f0fc8c4235
Pei-Tang Huang <tangtheone@gmail.com>
2017-05-27 12:46:31 +0800

    Change prod datbase from mariadb to postgresql.

--- src/main/java/com/tj/kvasir/domain/CategoryNode.java
+++ src/main/java/com/tj/kvasir/domain/CategoryNode.java
@@ -29,7 +29,8 @@ public class CategoryNode implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
+    @SequenceGenerator(name = "sequenceGenerator")
     private Long id;
 
     @NotNull

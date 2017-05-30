61035130e9b23645c9f1ac9d2e073a0e5fa9c402
Pei-Tang Huang <tangtheone@gmail.com>
2017-05-31 00:12:34 +0800

    Remove modules not used yet.

--- src/main/webapp/app/pool/category/category.module.ts
+++ src/main/webapp/app/pool/category/category.module.ts
@@ -4,13 +4,11 @@ import { KavsirSharedModule } from '../../shared/shared.module';
 import { RouterModule } from '@angular/router';
 import { categoryRoute } from './category.route';
 
-import { TreeModule } from 'angular-tree-component';
 import { CategoryAcademicYearService } from '../../entities/category-academic-year/category-academic-year.service';
 
 @NgModule({
     imports: [
         KavsirSharedModule,
-        TreeModule,
         RouterModule.forRoot(categoryRoute, {useHash: true}),
     ],
     providers: [

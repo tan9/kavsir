2396dc76963d9453ce9715bcd4d86a4f0f40fc00
Pei-Tang Huang <tangtheone@gmail.com>
2017-06-18 03:41:14 +0800

    Workaround tab content been lazy-loaded.

--- src/main/webapp/app/pool/category/categories.component.ts
+++ src/main/webapp/app/pool/category/categories.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Component, OnDestroy, OnInit, AfterViewInit, ViewChild } from '@angular/core';
 import { CategoryAcademicYearService } from '../../entities/category-academic-year';
 import { CategoryGradeService } from '../../entities/category-grade';
 import { CategorySemesterService } from '../../entities/category-semester';
@@ -8,19 +8,22 @@ import { CategoryAcademicYear } from '../../entities/category-academic-year/cate
 import { CategoryGrade } from '../../entities/category-grade/category-grade.model';
 import { CategorySemester } from '../../entities/category-semester/category-semester.model';
 import { CategoryPublisher } from '../../entities/category-publisher/category-publisher.model';
+import { NgbTabset } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
     selector: 'jhi-categories',
     templateUrl: './categories.component.html',
     styleUrls: ['./categories.component.css']
 })
-export class CategoriesComponent implements OnInit, OnDestroy {
+export class CategoriesComponent implements OnInit, OnDestroy, AfterViewInit {
 
     categoryAcademicYears: CategoryAcademicYear[] = [];
     categoryGrades: CategoryGrade[] = [];
     categorySemesters: CategorySemester[] = [];
     categorySubjects: CategorySubject[] = [];
     categoryPublishers: CategoryPublisher[] = [];
+
+    @ViewChild(NgbTabset) tabset: NgbTabset;
 
     constructor(public categoryAcademicYearService: CategoryAcademicYearService,
                 public categoryGradeService: CategoryGradeService,
@@ -30,6 +33,15 @@ export class CategoriesComponent implements OnInit, OnDestroy {
     }
 
     ngOnInit() {
+    }
+
+    ngAfterViewInit() {
+        // FIXME a dirty workaround to make tab been eager loaded.
+        this.tabset.select('tab-publishers');
+        this.tabset.select('tab-subjects');
+        this.tabset.select('tab-semesters');
+        this.tabset.select('tab-grades');
+        this.tabset.select('tab-academic-years');
     }
 
     ngOnDestroy() {

b3cc2b349fbf0722d7453ab781dc2db8ab197b48
Pei-Tang Huang <tangtheone@gmail.com>
2017-07-07 23:36:51 +0800

    Escape html.

--- src/main/webapp/app/shared/render/markup-render.component.ts
+++ src/main/webapp/app/shared/render/markup-render.component.ts
@@ -10,4 +10,25 @@ export class MarkupRenderComponent {
 
     @Input() text: string;
 
+    escape(text: string): string {
+        if (text) {
+            return text.replace(/(\\\[.*?\\])|(\\\(.*?\\\))|(<)|(>)/g,
+                (match, latexBlock, latexInline, lt, gt) => {
+                    if (latexBlock) {
+                        return latexBlock;
+                    }
+                    if (latexInline) {
+                        return latexInline;
+                    }
+                    if (lt) {
+                        return '&lt;';
+                    }
+                    if (gt) {
+                        return '&gt;';
+                    }
+                }).replace(/\n/g, '<br>');
+        }
+        return text;
+    }
+
 }

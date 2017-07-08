a46e07727c3c35529b128258f9723ff8a002c39b
Pei-Tang Huang <tangtheone@gmail.com>
2017-07-08 18:50:34 +0800

    Simplify regexp.

--- src/main/webapp/app/shared/render/markup-render.component.ts
+++ src/main/webapp/app/shared/render/markup-render.component.ts
@@ -12,21 +12,11 @@ export class MarkupRenderComponent {
 
     escape(text: string): string {
         if (text) {
-            return text.replace(/(\\\[.*?\\])|(\\\(.*?\\\))|(<)|(>)/g,
-                (match, latexBlock, latexInline, lt, gt) => {
-                    if (latexBlock) {
-                        return latexBlock;
-                    }
-                    if (latexInline) {
-                        return latexInline;
-                    }
-                    if (lt) {
-                        return '&lt;';
-                    }
-                    if (gt) {
-                        return '&gt;';
-                    }
-                }).replace(/\n/g, '<br>');
+            return text
+                .replace(
+                    /((?:\\\[.*?\\])|(?:\\\(.*?\\\)))|(<)|(>)/g,
+                    (match, latex, lt, gt) => latex || (lt && '&lt;') || (gt && '&gt;')
+                ).replace(/\n/g, '<br>');
         }
         return text;
     }

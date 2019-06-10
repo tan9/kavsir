fe20f970c60d3c67ff07526a1ef82eca9044183d
Pei-Tang Huang <tangtheone@gmail.com>
2019-06-11 07:55:43 +0800

    Change FontAwesome icon from WebFont to angular-fontawesome.

--- src/main/webapp/app/pool/category/category-select-popup.service.ts
+++ src/main/webapp/app/pool/category/category-select-popup.service.ts
@@ -4,38 +4,36 @@ import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 
 @Injectable()
 export class CategorySelectPopupService {
+  private isOpen = false;
 
-    private isOpen = false;
+  constructor(private modalService: NgbModal, private router: Router) {}
 
-    constructor(
-        private modalService: NgbModal,
-        private router: Router
-    ) {
+  open(component: Component, id?: number | any): NgbModalRef {
+    if (this.isOpen) {
+      return;
     }
+    this.isOpen = true;
 
-    open(component: Component, id?: number | any): NgbModalRef {
-        if (this.isOpen) {
-            return;
-        }
-        this.isOpen = true;
-
-        if (id) {
-            this.categorySelectModalRef(component, id);
-        } else {
-            return this.categorySelectModalRef(component);
-        }
+    if (id) {
+      this.categorySelectModalRef(component, id);
+    } else {
+      return this.categorySelectModalRef(component);
     }
+  }
 
-    categorySelectModalRef(component: Component, categorySelect?: number): NgbModalRef {
-        const modalRef = this.modalService.open(component, { size: 'lg', backdrop: 'static'});
-        modalRef.componentInstance.categorySelected = categorySelect;
-        modalRef.result.then((result) => {
-            this.router.navigate([{ outlets: { popup: null }}], { replaceUrl: true });
-            this.isOpen = false;
-        }, (reason) => {
-            this.router.navigate([{ outlets: { popup: null }}], { replaceUrl: true });
-            this.isOpen = false;
-        });
-        return modalRef;
-    }
+  categorySelectModalRef(component: Component, categorySelect?: number): NgbModalRef {
+    const modalRef = this.modalService.open(component, { size: 'lg', backdrop: 'static' });
+    modalRef.componentInstance.categorySelected = categorySelect;
+    modalRef.result.then(
+      result => {
+        this.router.navigate([{ outlets: { popup: null } }], { replaceUrl: true });
+        this.isOpen = false;
+      },
+      reason => {
+        this.router.navigate([{ outlets: { popup: null } }], { replaceUrl: true });
+        this.isOpen = false;
+      }
+    );
+    return modalRef;
+  }
 }
--- src/main/webapp/app/pool/category/category.component.ts
+++ src/main/webapp/app/pool/category/category.component.ts
@@ -6,62 +6,58 @@ import { Category } from '../../entities/category.model';
 import { CategoryService } from '../../entities/category.service';
 
 @Component({
-    selector: 'jhi-category',
-    templateUrl: './category.component.html'
+  selector: 'jhi-category',
+  templateUrl: './category.component.html'
 })
 export class CategoryComponent implements OnInit {
+  isSaving = false;
 
-    isSaving = false;
+  @Input() items: Category[];
 
-    @Input() items: Category[];
+  /** type name in CapitalizedCamelcase. */
+  @Input() typeName: string;
+  @Input() service: CategoryService<Category>;
 
-    /** type name in CapitalizedCamelcase. */
-    @Input() typeName: string;
-    @Input() service: CategoryService<Category>;
+  constructor(private eventManager: JhiEventManager, private alertService: JhiAlertService) {}
 
-    constructor(private eventManager: JhiEventManager,
-                private alertService: JhiAlertService) {
-    }
+  ngOnInit() {}
 
-    ngOnInit() {
-    }
+  hyphenSeparatedTypeName() {
+    return this.typeName.replace(/([A-Z])/g, g => `-${g[0].toLowerCase()}`).substring(1);
+  }
 
-    hyphenSeparatedTypeName() {
-        return this.typeName.replace(/([A-Z])/g, (g) => `-${g[0].toLowerCase()}`).substring(1);
-    }
+  move(index: number, direction: string) {
+    this.isSaving = true;
+    const target = index + (direction === 'UP' ? -1 : 1);
 
-    move(index: number, direction: string) {
-        this.isSaving = true;
-        const target = index + ((direction === 'UP') ? -1 : 1);
+    const tmp = this.items[index];
+    this.items[index] = this.items[target];
+    this.items[target] = tmp;
 
-        const tmp = this.items[index];
-        this.items[index] = this.items[target];
-        this.items[target] = tmp;
+    // TODO Observable?
+    const promises: Promise<HttpResponse<Category>>[] = [];
+    this.items.forEach((item, i) => {
+      if (item.position !== i) {
+        item.position = i;
+        promises.push(this.service.update(item).toPromise());
+      }
+    });
 
-        // TODO Observable?
-        const promises: Promise<HttpResponse<Category>>[] = [];
-        this.items.forEach((item, i) => {
-            if (item.position !== i) {
-                item.position = i;
-                promises.push(this.service.update(item).toPromise());
-            }
-        });
+    const eventName = 'category' + this.typeName + 'ListModification';
+    Promise.all(promises).then(
+      () => {
+        this.eventManager.broadcast(eventName);
+        this.isSaving = false;
+      },
+      error => {
+        this.onError(error);
+        this.eventManager.broadcast(eventName);
+        this.isSaving = false;
+      }
+    );
+  }
 
-        const eventName = 'category' + this.typeName + 'ListModification';
-        Promise.all(promises).then(
-            () => {
-                this.eventManager.broadcast(eventName);
-                this.isSaving = false;
-            },
-            (error) => {
-                this.onError(error);
-                this.eventManager.broadcast(eventName);
-                this.isSaving = false;
-            }
-        );
-    }
-
-    private onError(error) {
-        this.alertService.error(error.message, null, null);
-    }
+  private onError(error) {
+    this.alertService.error(error.message, null, null);
+  }
 }
